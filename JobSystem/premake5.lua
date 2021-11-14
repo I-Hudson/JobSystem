ead5c6b44d36a807f1cd135ad841d285df15b71d
I-Hudson <cuffy94@gmail.com>
2021-11-14 11:59:37 +0000

    Changed bin dir.

--- JobSystem/premake5.lua
+++ JobSystem/premake5.lua
@@ -4,9 +4,9 @@ project "JobSystem"
     cppdialect "C++17"
 	staticruntime "on"
 
-    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
-    debugdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    targetdir ("../bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("../bin-int/" .. outputdir .. "/%{prj.name}")
+    debugdir ("../bin/" .. outputdir .. "/%{prj.name}")
 
     files
 	{
